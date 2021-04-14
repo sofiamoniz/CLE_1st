c26d1351ec677f1755acb0fa20b2d290a4145a86
Sofia Moniz <sofiamoniz@ua.pt>
2021-04-14 17:12:37 +0100

    functions

--- dataSet1/count.c
+++ dataSet1/count.c
@@ -1,25 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//correr 
-//gcc count.c   
-//gcc -o count count.c  
-//./count text0.txt text1.txt text2.txt text3.txt text4.txt
-int check_vowel(char a)
-{
-  char no[] = "ÁÀÂÃáàâãÉÈÊéèêÍÌíìÓÒÔÕóòôõÚÙúùÇçyY";
-  if (a == 'A' || a == 'E' || a == 'I' || a == 'O' || a == 'U')
-    return 1;
-  else if (a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u')
-    return 1;
-  else    // You may omit this else as the control comes here if the character is not a vowel.
-    for (int i = 0; strlen(no);++i){
-        if (a != no[i]){
-            return 0;
-        }
-    }
-    return 1;
-}
+#include "functions.h"
 
 int main(int argc, char *argv[])
 {
@@ -28,6 +10,7 @@ int main(int argc, char *argv[])
     char word[50];
     int wrd ;
     int consonant = 0;
+    int len = get_size("~");
     
 
     //printf("Enter file name:\n");
@@ -42,35 +25,16 @@ int main(int argc, char *argv[])
             while (fscanf(in_file, "%s", word) != EOF)
             {
                 //printf("%s\n", word);
-                ++wrd;
+                //++wrd;
                 for (int i = 0; word[i] != '\0';++i){
                     //printf("(%c)", word[i]);
-                    if ((word[i] >= 'a' && word[i] <= 'z') || (word[i] >= 'A' && word[i] <= 'Z')) {
-                        if (check_vowel(word[i]) == 0){
-                            //printf("(%c)", word[i]);
-                            consonant++;
-                        }
-                    }
+                    printf("The size is: %d", len);
                     
-
-                    
-                        /*if ((word[i] >= 'a' && word[i] <= 'z') || (word[i] >= 'A' && word[i] <= 'Z')) {
-                            for(int j=0; j<strlen(no);j++){
-                                if(word[i] != no[j]){
-                                    ++consonant;
-                                }
-                            }
-                            
-                        }
-                        */
                     
                     
                 }
 
             }
-            printf("\n The number of words in the  file %s is : %d\n",argv[i],wrd-1); 
-            printf("\n The number of consonants in the  file %s is : %d\n",argv[i],consonant); 
-            fclose(in_file);
         }
     }
     return 0;
--- dataSet1/functions.h
+++ dataSet1/functions.h
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <locale.h>
 
 //Check if is vowel | 1-> is vowel
 int is_vowel(unsigned char c){
     if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='y'){
         return 1;
     }
-    else if(c=='A' || c=='E' || c=='I' || c=='O' || c=='Y'){
+    else if(c=='A' || c=='E' || c=='I' || c=='O' || c=='U' || c=='Y'){
         return 1;
     }
     else{
@@ -15,40 +16,133 @@ int is_vowel(unsigned char c){
     }
 }
 
-//Check if is alpha | 1-> is alpha
-int check_alpha(unsigned char c){
+//Check if is alpha or underscore | 1-> is alpha or underscore
+int is_alpha_underscore(unsigned char c){
     if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
         return 1;
     }
     else if((c>='0')&&(c<='9')){
         return 1;
     }
-    //else if(c=='_'){ //porque é q isto está aqui
-      //  return 1;
-    //}
-    else{
-        return 0;
-    }
-}
-
-int check_space_separation_apostrophe(unsigned char c){
-    if(c==' ' || c =='  ' || c==0xa){
-        return 1;
-    }
-    else if(c=='-' || c=='_' || c=='"' || c == '[' || c ==']' || c=='(' || c==')'){
-        return 1;
-    }
-    else if(c=='.' || c == ',' || c==':' || c==';' || c == '?' || c =='!' || c == 0xE28093 || c == 0xE280A6 ){
+    else if(c=='_'){
         return 1;
     }
     else{
         return 0;
     }
 }
-//the apostrophe (' 0x27) and single quotation marks (‘ 0xE28098 - ’ 0xE28099)
-//are considered here to merge two words into a single one.
-//rip
-int symbol_transformation(unsigned char **c, int length){
 
+//Check if ia space, separation or punctiation
+int is_space_separation_punctuation(unsigned char c){
+    if(c==' ' || c =='  ' || c==0xa){ //space
+        return 1;
+    }
+    else if(c=='-' || c=='"' || c == '[' || c ==']' || c=='(' || c==')'){ //separation
+        return 1;
+    }
+    else if(c=='.' || c == ',' || c==':' || c==';' || c == '?' || c =='!' || c == 0xE28093 || c == 0xE280A6 ){ //punctuation
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
 
+//Check if is apostrophe
+int is_apostrophe(unsigned char c){
+    if(c==0x27){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+//tirei daqui https://stackoverflow.com/questions/33737803/how-to-compare-multibyte-characters-in-c
+//get size of bytes read of utf-8 char
+int get_size(char * c){
+    int i=0, char_len;
+
+    setlocale(LC_CTYPE, "en_US.utf8");
+
+    while ((char_len = mblen(&c[i], MB_CUR_MAX)) > 0)
+    {
+        /* &text[i] contains multibyte character of length char_len */
+        if(memcmp(&c[i], "ö", char_len) == 0)
+        {
+            printf("ö \n");
+        }
+
+        i += char_len;
+    }
+
+    return i;
+}
+//convert multi byte to single byte
+unsigned char conversor_multibyte(char ** c, int char_len){
+    if(char_len == 1){
+        unsigned char c_first = (*c)[0]; //first and only part of char, in this case
+        if (c_first == '´' || c_first == '`'){ //if the char is equal to left or right single
+                                            //quotion marks, it should be converted to apostrophe
+            return 0x27;
+        }
+    }
+    else if(char_len == 2){
+        unsigned char c_first = (*c)[0];
+        unsigned char c_second = (*c)[1];
+        if(c_first == 0xc3){
+            //Á–À–Â–Ã–á–à–â–ã to A :
+            if(c_second == 0x80 || c_second == 0x81 || c_second == 0x82 || c_second == 0x83 ||
+                c_second == 0xa0 || c_second == 0xa1 || c_second == 0xa2 || c_second == 0xa3){
+                return 'A';
+            }
+            //É–È–Ê–é–è–ê to E :
+            else if(c_second == 0x88 || c_second == 0x89 || c_second == 0x8a ||
+                c_second == 0xa8 || c_second == 0xa9 || c_second == 0xaa){
+                return 'E';
+            }
+            //Í–Ì–í–ì to I
+            else if(c_second == 0x8c || c_second == 0x8d ||
+                c_second == 0xac || c_second == 0xad ){
+                return 'I';
+            }
+            //Ó–Ò–Ô–Õ–ó–ò–ô–õ
+            else if(c_second == 0x92 || c_second == 0x93 || c_second == 0x94 || c_second == 0x95 ||
+                c_second == 0xb2 || c_second == 0xb3 || c_second == 0xb4 || c_second == 0xb5){
+                return 'O';
+            }
+            //Ú–Ù–ú–ù to U
+            else if(c_second == 0x99 || c_second == 0x9a ||
+                c_second == 0xb9 || c_second == 0xba ){
+                return 'U';
+            }
+            //Ç–ç to C
+            else if(c_second == 0x87 || c_second == 0xa7){
+                return 'C';
+            }
+        }
+        //left and right double quotation marks to double quotation mark
+        else if(c_first == 0xc2){
+            if(c_second == 0xab || c_second == 0xbb){
+                return '"';
+            }
+        }
+    }
+    else if(char_len == 3){
+        unsigned char c_first = (*c)[0];
+        unsigned char c_second = (*c)[1];
+        unsigned char c_third = (*c)[2];
+        //dash to hyphen
+        if((c_first== 0xe2 && c_second == 0x80 ) && (c_third == 0x92 || c_third == 0x93)){
+            return '-';
+        }
+        //ellipsis to full point
+        else if(c_first == 0xe2 && c_second == 0x80 && c_third == 0xa6){
+            return '.';
+        }
+    }
+    else{
+        printf("Error reading char.\n");
+        exit(1);
+    }
+}
